980c9920351d199d610c2a3d4a83fc45fb4aca02
cod3ctq <onlyforcodectq1@gmail.com>
2024-04-19 10:21:15 -0600

    proyecto_xxx

---
+++ estructuras_control/src/com/Principal.java
@@ -0,0 +1,87 @@
+package com;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		// if
+
+		String x = "hola";
+
+		// System.out.println(x.length()>10 && x.equals("holaholahola"));
+
+		// System.out.println(x.length());
+		// System.out.println(x.equals("holaholahola"));
+
+//		//AND
+//		//       true          true                      false
+//		if(x.length()>10 && x.equals("holaholahola") && x.isEmpty()) {
+//			System.out.println("OK");
+//			
+//		}else {
+//			System.out.println("Nop");
+//		}
+
+//		//OR
+//		//   true     
+//		if(x.length()>10 || x.equals("hola")) {
+//			System.out.println("OK");			
+//		}else {
+//			System.out.println("Nop");
+//		}
+
+//		if(!(x.equals("hola"))) {
+//			System.out.println("OK");			
+//		}else {
+//			System.out.println("Nop");
+//		}
+
+//		if(x.equals("HOLA")) {
+//			System.out.println("Uno");
+//		}else if(x.equals("Hola")) {
+//			System.out.println("Dos");
+//		}else if(x.equals("holA")) {
+//			System.out.println("Tres");
+//		}else if(x.equals("HoLa")) {
+//			System.out.println("4");
+//		}else if(x.equals("HOla")) {
+//			System.out.println("5");
+//		}else {
+//			System.out.println("Nel");
+//		}
+
+//		switch (x) {
+//
+//		case "HoLa":
+//
+//			break;
+//		case "HOLA":
+//
+//			break;
+//		case "HOLa":
+//
+//			break;
+//		case "holA":
+//
+//			break;
+//		case "hoLa":
+//
+//			break;
+//		case "hOLA":
+//			
+//			break;
+//		case "hola":
+//			
+//			break;
+//
+//			default:
+//				//Aqui haces lo que queda
+//			
+//
+//		}
+		
+	}
+
+}
