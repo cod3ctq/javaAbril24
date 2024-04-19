d26bd0e1a7e6333a6731e9c2c818e5d04a3fb4dc
JAIMEURIEL1993 <ta_0123@hotmail.com>
2024-04-19 10:21:21 -0600

    proyecto_juml

---
+++ estructuras_control/src/com/Principal.java
@@ -0,0 +1,71 @@
+package com;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// if
+		String x = "Hola";
+//		System.out.println(x.length() > 10 && x.equals("hola"));
+//		System.out.println(x.length());
+//		System.out.println("hola");
+
+		// Operador AND
+
+//		if (x.length() < 10 && x.equals("hola")) {
+//			System.out.println("ok");
+//		} else {
+//			System.out.println("no");
+//		}
+
+		// Operador OR
+
+//		if (x.length() < 10 || x.equals("hola")) {
+//			System.out.println("ok");
+//		} else {
+//			System.out.println("no");
+//		}
+		
+		// Operador NOT
+//		if (!(x.equals("hola"))) {
+//			System.out.println("ok");
+//		}else {
+//			System.out.println("no");
+//		}
+		
+//		if(x.equals("HOLA")) {
+//			System.out.println("ok");
+//		}else if(x.equals("wlkejd")) {
+//			System.out.println("ok");
+//		}else if(x.equals("HOLAwewe3")) {
+//			System.out.println("ok");
+//		}else if(x.equals("HOLAw328y98dehoi"))) {
+//			System.out.println("ok");
+//		}else {
+//			System.out.println("nop");
+//		}
+		
+		switch(x){
+		
+		case "HOLA":
+			break;
+		case "HOLa":
+			break;
+		case "HOla":
+			break;
+		case "Hola":
+			break;
+		case "hOLA":
+			break;
+		case "hoLA":
+			break;
+		case "holA":
+			break;
+		case "hola":
+			break;
+			
+			default:
+		}
+		
+	}
+
+}
